024dce5b38862fa34cf93009751664b72a6210ad
Chris Stevens <chris.stevens@databricks.com>
2023-09-13 20:43:03 -0400

    Update comments in Python code.

--- custom-api/store_server.py
+++ custom-api/store_server.py
@@ -13,14 +13,17 @@ from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.sql import *
 
 # Initialize Databricks SDK. 
-# By default, this authenticates using DATABRICKS_HOST and DATABRICKS_TOKEN environment variables.
+# By default, this authenticates using the DATABRICKS_HOST and DATABRICKS_TOKEN environment variables
+# initialized by setup.sh.
 w = WorkspaceClient()
 
 warehouse_id = os.environ.get("DATABRICKS_WAREHOUSE_ID")
 
 app = Flask(__name__)
 
+#
 # Store the parameterized SQL statements used to read/write data to Databricks SQL.
+#
 class SqlStatement(Enum):
     LIST_STORES = 1
     LIST_SALES = 2
@@ -59,7 +62,9 @@ sql_statements = {
         """
 }
 
+#
 # Web Endpoints
+#
 @app.route("/")
 def index():
     return render_template("stores.html")
@@ -72,9 +77,17 @@ def customer(store_id):
 def newSale():
     return render_template("new_sale.html")
 
-# API Endpoints
+#
+# Custom API Endpoints
+#
 
+#
 # Endpoint to list store information.
+#
+# It takes no arguments and invokes the Statement Execution API in synchronous mode, returning store data results in
+# JSON format. If the request to Databricks SQL takes longer than 50 seconds, the execution will be cancelled and no
+# data is returned.
+#
 @app.route("/api/1.0/stores", methods=["GET"])
 def list_stores():
     statement_response = w.statement_execution.execute_statement(
@@ -97,6 +110,25 @@ def list_stores():
     
     return jsonify(response)
 
+#
+# Endpoint to list sales for a specific store.
+#
+# This invokes the Statement Execution API in asynchronous mode, returning an ID the client can use to poll on status.
+# Once the status is SUCCEEDED, the number of result chunks is returned to the caller. Then, in a parallel, the caller
+# can get external links for each chunk and download the data from cloud storage.
+#
+# Arguments:
+#   store_id - Required ID of the store supplied in the URL.
+#   request_id - Optional ID used to poll for status on a request.
+#   chunk_index - Optional chunk index that can be used to get an external link to data for a successful request.
+#
+# Return Values:
+#   request_id - Returned for all successfully accepted requests. Used this ID to poll for completion status.
+#   state - Returned on the initial request and status polls. Can be PENDING, RUNNING, SUCCEEDED, FAILED, CANCELED,
+#           or CLOSED.
+#   chunk_count - Returned for successful requests, indicating how many chunks of sales data there are for the store.
+#   link - A pre-signed URL returned when a request_id and chunk_index are supplied as arguments.
+#   
 @app.route("/api/1.0/stores/<string:store_id>/sales", methods=["GET"])
 def list_sales_async(store_id):
     # If a request ID is not supplied, execute a new list request.
@@ -118,6 +150,7 @@ def list_sales_async(store_id):
         
     return jsonify(response)
 
+# Invoke the Statement Execution API in asynchronous mode, returning an ID so the client can poll on status.
 def execute_list_sales_request(store_id, format, row_limit):
     # Use parameters to prevent SQL injection via the store ID string.
     parameters = [
@@ -142,6 +175,7 @@ def execute_list_sales_request(store_id, format, row_limit):
 
     return response
 
+# Poll the Statement Execution API for status on a previously generated execution request.
 def get_list_sales_status(request_id):
     statement_response = w.statement_execution.get_statement(statement_id = request_id)
     response = {
@@ -154,6 +188,7 @@ def get_list_sales_status(request_id):
 
     return response
 
+# Get the external link for the given chunk. The pre-signed URL is valid for 15 mins.
 def get_list_sales_chunk(request_id, chunk_index):
     result_data = w.statement_execution.get_statement_result_chunk_n(
         statement_id = request_id,
@@ -167,6 +202,18 @@ def get_list_sales_chunk(request_id, chunk_index):
 
     return response
 
+#
+# Endpoint to add a sale to a store.
+#
+# This endpoint uses a parameterized query to insert data in to the Lakehouse via the Statement Execution API.
+#
+# Arguments:
+#   store_id - Required ID of the store supplied in the URL.
+#   date - Required date of the day the sale took place.
+#   price - Required price of each item sold.
+#   quantity - Required number of items sold.
+#   item-id - Required ID for the item sold.
+#   
 @app.route("/api/1.0/stores/<string:store_id>/sales", methods=["POST"])
 def insert_sale(store_id):
     sold_date = request.form.get('date')
@@ -197,10 +244,12 @@ def insert_sale(store_id):
     }
 
     return jsonify(response)
-    
+
+#
 # WARNING: Don't use in production.
 # 
-# The following is needed to overcome CORS without workspace bucket modifications.
+# The following is needed to overcome CORS without Databricks workspace bucket modifications.
+#
 local_cors_proxy_lock = threading.Lock()
 local_cors_proxy_enabled = False
 
